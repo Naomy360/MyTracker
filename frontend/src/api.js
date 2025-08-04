e53a5cee8f85f04038ecda4a78d25738800a33e1
Naomy Chepngeno <85355678+Naomy360@users.noreply.github.com>
2025-08-04 02:32:52 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://mytracker-rsxn.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5050/api',
 });
 
 export default API;
